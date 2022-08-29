c20d93e71179c2265dbf656b1244e6a26f26ad10
Jay Ring-Sakabe <jrins@Jays-MacBook-Pro.local>
2022-08-29 10:50:14 +0900

    pseudo coded plan on server.js

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(express.urlencoded( {extended: true}))
 //Step 4: Set up the static Middleware.
 app.use (express.static('public'))
 
+//Step 5: Set up routes to get in information from the Database
+
+//Step 6: Set up routes and methods to be able to post information and save it to the Database
+
+//Step 7: Set up method that allows a user to remove an entry in the Database.
+
+
 
 //Part of Step 3
 app.listen(PORT, () => console.log(`App listening on the following port: ${PORT}`))
