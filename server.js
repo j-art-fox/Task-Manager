f2909dc968a75d60af5183a4f4ba9c25a42f4a72
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-08-31 20:32:08 +0900

    cleaned up, commented on,  and organized the server.js file because this assignment was very difficult and I wanted to review very specifically what everything does.

--- server.js
+++ server.js
@@ -1,38 +1,26 @@
-// Step 1: Set up Dependencies
+//Utilizes express to set up server, and sets it to the variable "app" so we can use it later in our middleware and to create a "crud" app.
 const express = require("express");
-
-const apiroutes = require('./routes/apiroutes')
-const htmlroutes = require('./routes/htmlroutes')
-
-//Step 3 Set up the server.
 const app = express();
-const PORT = process.env.PORT || 3001
 
-// these are middleware data parsing methods from the client. express.json is for post requests
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api', apiroutes)
-app.use('/', htmlroutes)
+//Creates new variable that requires express and new instances of router for both the api and html calls.
+const apiroutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes');
 
-//Step 4: Set up the static Middleware.
+//Tells the app to listen for activity locally on port 3101 or through whatever port is determined by heroku.
+const PORT = process.env.PORT || 3101;
 
+//These are middleware data parsing methods from the client. express.json is for post requests
+app.use(express.static('public'));
 
+//Built in middleware for post/put requests for sending data in the form of an object to the serer that you are attempting to store in the req.body. 
+//Express.urlencoded is a method that recognizes the incoming request object as a string/array
+app.use(express.urlencoded({ extended: true }));
+//Express.json recognizes incoming requests which are JSON objects
+app.use(express.json());
 
-// "crud" create(post) read(get) update (put) delete,
+//Tells the server to use the following routes with our application
+app.use('/api', apiroutes);
+app.use('/', htmlroutes);
 
-
-//Step 6: Set up routes and methods to be able to post information and save it to the Database
-
-
-
-
-//Step 7: Set up method that allows a user to remove an entry in the Database.
-
-// app.delete(`/api/notes/:id`, (req, res) => {
-//     data = JSON.parse(fs.readFileSync('./db/db.json', "utf8",))
-   
-// })
-
-//Part of Step 3
+//listens to activity coming through in the PORT variable.
 app.listen(PORT, () => console.log(`App listening on the following port: ${PORT}`))
